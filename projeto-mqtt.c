120165774034d8fb9a2a7f8adff79fb2b9e49a18
Matheus Nepomuceno Souza <mnepomuceno2004@gmail.com>
2025-06-04 16:24:38 -0300

    adição de comentários específicos

--- projeto-mqtt.c
+++ projeto-mqtt.c
@@ -24,6 +24,9 @@ double ph = 7.0;           // Começa 7.0 de ph
 
 #define STEP 0.01 // Passo de variação para cada movimento do joystick
 
+// pino do botão
+#define botaoB 6
+
 // matrizes que definem quais leds acendem
 bool escritorio[NUM_PIXELS] = {
     1, 1, 0, 0, 0,
@@ -137,9 +140,6 @@ static void pub_request_cb(__unused void *arg, err_t err);
 // Topico MQTT
 static const char *full_topic(MQTT_CLIENT_DATA_T *state, const char *name);
 
-// Controle do LED
-static void control_led(MQTT_CLIENT_DATA_T *state, bool on);
-
 // Controle da matriz (simulação dos cômodos)
 static void control_lights(MQTT_CLIENT_DATA_T *state, bool on, bool *comodo);
 
@@ -174,8 +174,21 @@ static void start_client(MQTT_CLIENT_DATA_T *state);
 // Call back com o resultado do DNS
 static void dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg);
 
+// Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    reset_usb_boot(0, 0);
+}
 int main(void)
 {
+
+    // inicializa e configura o botão
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    // Aqui termina o trecho para modo BOOTSEL com botão B
 
     // Inicializa todos os tipos de bibliotecas stdio padrão presentes que estão ligados ao binário.
     stdio_init_all();
@@ -274,16 +287,21 @@ int main(void)
         joystick_leitura_corrigida(&dx, &dy);
         // printf("Temperatura X: %.2f, PH Y: %.2f\n", temperatura, ph);
 
-        // Ajuste de temperatura (eixo X)
+        // Ajuste de temperatura com base no movimento no eixo X
+        // Se o movimento for maior que 1000 altera a temperatura
         if (abs(dx) > 1000)
         {
+            // Aumenta ou diminui a temperatura com base na direção do movimento
             temperatura += (dx > 0) ? STEP : -STEP;
         }
 
-        // Ajuste de ph (eixo Y)
+        // Ajuste de pH com base no movimento no eixo Y
         if (abs(dy) > 1000)
         {
+            // Calcula o novo valor de pH antes de aplicar
             float novo_ph = ph + ((dy > 0) ? STEP : -STEP);
+
+            // Garante que o pH fique dentro da faixa válida (0 a 14)
             if (novo_ph >= 0 && novo_ph <= 14)
             {
                 ph = novo_ph;
@@ -316,82 +334,135 @@ static const char *full_topic(MQTT_CLIENT_DATA_T *state, const char *name)
 #endif
 }
 
+// Função que controla as luzes (LEDs) de um cômodo e publica o estado no MQTT.
+// Parâmetros:
+// - state: estrutura com dados do cliente MQTT
+// - on: indica se a luz deve ser ligada (true) ou desligada (false)
+// - comodo: ponteiro que identifica qual cômodo está sendo controlado
 static void control_lights(MQTT_CLIENT_DATA_T *state, bool on, bool *comodo)
 {
-    // Publish state on /state topic and on/off led board
+    // Define a mensagem a ser publicada com base no estado (ligado/desligado)
     const char *message = on ? "On" : "Off";
+
+    // Se a luz deve ser LIGADA
     if (on)
     {
         if (comodo == garagem)
         {
-            atualizarLeds(garagem, true);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/garagem/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(garagem, true); // Liga os LEDs da garagem
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/garagem/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == sala)
         {
-            atualizarLeds(sala, true);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/sala/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(sala, true); // Liga os LEDs da sala
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/sala/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == quarto)
         {
-            atualizarLeds(quarto, true);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/quarto/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(quarto, true); // Liga os LEDs do quarto
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/quarto/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == escritorio)
         {
-            atualizarLeds(escritorio, true);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/escritorio/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(escritorio, true); // Liga os LEDs do escritório
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/escritorio/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
     }
+    // Caso contrário, a luz deve ser DESLIGADA
     else
     {
         if (comodo == garagem)
         {
-            atualizarLeds(garagem, false);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/garagem/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(garagem, false); // Desliga os LEDs da garagem
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/garagem/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == sala)
         {
-            atualizarLeds(sala, false);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/sala/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(sala, false); // Desliga os LEDs da sala
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/sala/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == quarto)
         {
-            atualizarLeds(quarto, false);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/quarto/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(quarto, false); // Desliga os LEDs do quarto
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/quarto/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
         else if (comodo == escritorio)
         {
-            atualizarLeds(escritorio, false);
-            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/escritorio/state"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+            atualizarLeds(escritorio, false); // Desliga os LEDs do escritório
+            mqtt_publish(state->mqtt_client_inst, full_topic(state, "/escritorio/state"), message, strlen(message),
+                         MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
         }
     }
-    return;
+
+    return; // Retorno explícito, embora desnecessário para função void
 }
 
-// Publicar temperatura
+// Função responsável por publicar os dados de temperatura e pH no MQTT,
+// apenas se houver mudança nos valores em relação à última publicação.
 static void publish_condition(MQTT_CLIENT_DATA_T *state)
 {
     static float old_temperatura;
     const char *temperature_key = full_topic(state, "/aquario/temperature");
+
+    // Verifica se a temperatura atual mudou em relação à última publicada
     if (temperatura != old_temperatura)
     {
+        // Atualiza o valor da última temperatura publicada
         old_temperatura = temperatura;
+
+        // Converte o valor da temperatura para string com uma casa decimal
         char temp_str[16];
         snprintf(temp_str, sizeof(temp_str), "%.1f", temperatura);
+
+        // Imprime no log a mensagem que será publicada
         INFO_printf("Publishing %s to %s\n", temp_str, temperature_key);
-        mqtt_publish(state->mqtt_client_inst, temperature_key, temp_str, strlen(temp_str), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+
+        // Publica a temperatura no broker MQTT
+        mqtt_publish(
+            state->mqtt_client_inst, // Instância do cliente MQTT
+            temperature_key,         // Tópico onde será publicada a temperatura
+            temp_str,                // Payload (valor da temperatura como string)
+            strlen(temp_str),        // Tamanho do payload
+            MQTT_PUBLISH_QOS,        // Qualidade de serviço (QoS) da publicação
+            MQTT_PUBLISH_RETAIN,     // Flag de retenção da mensagem
+            pub_request_cb,          // Callback de confirmação de publicação
+            state                    // Contexto do cliente
+        );
     }
 
     static float old_ph;
     const char *ph_key = full_topic(state, "/aquario/ph");
+    // Verifica se o valor de pH atual mudou em relação ao último publicado
     if (ph != old_ph)
     {
+        // Atualiza o valor do último pH publicado
         old_ph = ph;
+
+        // Converte o valor do pH para string com uma casa decimal
         char ph_str[16];
         snprintf(ph_str, sizeof(ph_str), "%.1f", ph);
+
+        // Imprime no log a mensagem que será publicada
         INFO_printf("Publishing %s to %s\n", ph_str, ph_key);
-        mqtt_publish(state->mqtt_client_inst, ph_key, ph_str, strlen(ph_str), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+
+        // Publica o valor de pH no broker MQTT
+        mqtt_publish(
+            state->mqtt_client_inst, // Instância do cliente MQTT
+            ph_key,                  // Tópico onde será publicado o pH
+            ph_str,                  // Payload (valor do pH como string)
+            strlen(ph_str),          // Tamanho do payload
+            MQTT_PUBLISH_QOS,        // Qualidade de serviço (QoS) da publicação
+            MQTT_PUBLISH_RETAIN,     // Flag de retenção da mensagem
+            pub_request_cb,          // Callback de confirmação de publicação
+            state                    // Contexto do cliente
+        );
     }
 }
 
@@ -503,43 +574,69 @@ static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len
     strncpy(state->topic, topic, sizeof(state->topic));
 }
 
-// Publicar condições
+// Função executada periodicamente para publicar as condições
 static void condition_worker_fn(async_context_t *context, async_at_time_worker_t *worker)
 {
+    // Recupera os dados do cliente MQTT a partir do campo user_data do worker
     MQTT_CLIENT_DATA_T *state = (MQTT_CLIENT_DATA_T *)worker->user_data;
+
+    // Chama a função que verifica e publica as condições (se houver mudanças)
     publish_condition(state);
-    async_context_add_at_time_worker_in_ms(context, worker, TEMP_WORKER_TIME_S * 1000);
+
+    // Agenda a próxima execução desta função após TEMP_WORKER_TIME_S segundos
+    async_context_add_at_time_worker_in_ms(
+        context,                  // Contexto assíncrono usado para agendamento
+        worker,                   // Worker que será executado novamente
+        TEMP_WORKER_TIME_S * 1000 // Tempo em milissegundos (conversão de segundos)
+    );
 }
 
-// Conexão MQTT
+// Callback chamado após tentativa de conexão MQTT (sucesso ou falha)
 static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status)
 {
+    // Recupera a estrutura de dados do cliente MQTT passada como argumento
     MQTT_CLIENT_DATA_T *state = (MQTT_CLIENT_DATA_T *)arg;
+
+    // Verifica se a conexão foi aceita com sucesso pelo broker
     if (status == MQTT_CONNECT_ACCEPTED)
     {
         state->connect_done = true;
-        sub_unsub_topics(state, true); // subscribe;
+        sub_unsub_topics(state, true);
 
-        // indicate online
         if (state->mqtt_client_info.will_topic)
         {
-            mqtt_publish(state->mqtt_client_inst, state->mqtt_client_info.will_topic, "1", 1, MQTT_WILL_QOS, true, pub_request_cb, state);
+            mqtt_publish(
+                state->mqtt_client_inst,            // Cliente MQTT
+                state->mqtt_client_info.will_topic, // Tópico de presença
+                "1",                                // Payload indicando "online"
+                1,                                  // Tamanho do payload
+                MQTT_WILL_QOS,                      // QoS da publicação
+                true,                               // Retenção da mensagem
+                pub_request_cb,                     // Callback após publicação
+                state                               // Contexto do cliente
+            );
         }
 
-        // Publish temperature every 10 sec if it's changed
+        // Inicia o agendador assíncrono para publicar temperatura/pH periodicamente
         condition_worker.user_data = state;
-        async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(), &condition_worker, 0);
+        async_context_add_at_time_worker_in_ms(
+            cyw43_arch_async_context(), // Contexto assíncrono
+            &condition_worker,          // Worker que será executado periodicamente
+            0                           // Executar imediatamente a primeira vez
+        );
     }
+    // Caso o cliente tenha sido desconectado antes da conexão ser concluída
     else if (status == MQTT_CONNECT_DISCONNECTED)
     {
         if (!state->connect_done)
         {
-            panic("Failed to connect to mqtt server");
+            panic("Failed to connect to mqtt server"); // Aborta o programa com erro
         }
     }
+    // Se o status recebido for inesperado, trata como erro crítico
     else
     {
-        panic("Unexpected status");
+        panic("Unexpected status"); // Aborta o programa com erro
     }
 }
 
@@ -575,17 +672,22 @@ static void start_client(MQTT_CLIENT_DATA_T *state)
     cyw43_arch_lwip_end();
 }
 
-// Call back com o resultado do DNS
+// Callback chamado quando a resolução de DNS é concluída (sucesso ou falha)
 static void dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg)
 {
     MQTT_CLIENT_DATA_T *state = (MQTT_CLIENT_DATA_T *)arg;
+
     if (ipaddr)
     {
+        // Armazena o endereço IP do servidor MQTT na estrutura de estado
         state->mqtt_server_address = *ipaddr;
+
+        // Inicia o cliente MQTT com o endereço IP obtido
         start_client(state);
     }
     else
     {
+        // Se a resolução de DNS falhou, gera um erro crítico e aborta
         panic("dns request failed");
     }
 }
